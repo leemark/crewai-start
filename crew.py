d1a6748321e4fb1b3f11d77bf2a72683a876724f
Mark Lee <leemark@gmail.com>
2024-02-12 22:57:10 -0700

    added streamlit ui

--- crew.py
+++ crew.py
@@ -1,8 +1,9 @@
 from crewai import Agent
 from langchain_openai import ChatOpenAI
+import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
-
+st.title('🤖 Ask a Crew.')
 llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125")
 
 from langchain_community.llms import DeepInfra
@@ -14,73 +15,78 @@ llm_di.model_kwargs = {
     "top_p": 0.9,
 }
 
-# Topic that will be used in the crew run
-topic = 'AI in healthcare'
+def generate_response(topic):
 
-# Creating a senior researcher agent
-researcher = Agent(
-  role='Senior Researcher',
-  goal=f'Uncover groundbreaking technologies around {topic}',
-  verbose=True,
-  backstory="""Driven by curiosity, you're at the forefront of
-  innovation, eager to explore and share knowledge that could change
-  the world.""",
-  llm = llm
-)
+  # Creating a senior researcher agent
+  researcher = Agent(
+    role='Senior Researcher',
+    goal=f'Uncover groundbreaking news and insights around {topic}',
+    verbose=True,
+    backstory="""Driven by curiosity, you're at the forefront of
+    innovation, eager to explore and share knowledge that could change
+    the world.""",
+    llm = llm
+  )
 
-# Creating a writer agent
-writer = Agent(
-  role='Writer',
-  goal=f'Narrate compelling tech stories around {topic}',
-  verbose=True,
-  backstory="""With a flair for simplifying complex topics, you craft
-  engaging narratives that captivate and educate, bringing new
-  discoveries to light in an accessible manner.""",
-  llm = llm
-)
+  # Creating a writer agent
+  writer = Agent(
+    role='Writer',
+    goal=f'Narrate compelling stories around the requestion question or topic: {topic}',
+    verbose=True,
+    backstory="""With a flair for simplifying complex topics, you craft
+    engaging narratives that captivate and educate, bringing new
+    discoveries to light in an accessible manner.""",
+    llm = llm
+  )
 
-from crewai import Task
+  from crewai import Task
 
-# Install duckduckgo-search for this example:
-# !pip install -U duckduckgo-search
+  # Install duckduckgo-search for this example:
+  # !pip install -U duckduckgo-search
 
-from langchain_community.tools import DuckDuckGoSearchRun
-search_tool = DuckDuckGoSearchRun()
+  from langchain_community.tools import DuckDuckGoSearchRun
+  search_tool = DuckDuckGoSearchRun()
 
-# Research task for identifying AI trends
-research_task = Task(
-  description=f"""Identify the next big trend in {topic}.
-  Focus on identifying pros and cons and the overall narrative.
+  # Research task for identifying trends
+  research_task = Task(
+    description=f"""Identify the current developments in {topic}.
+    Focus on identifying trends and the overall narrative.
 
-  Your final report should clearly articulate the key points,
-  its market opportunities, and potential risks.
-  """,
-  expected_output='A comprehensive 3 paragraphs long report on the latest AI trends.',
-  max_inter=3,
-  tools=[search_tool],
-  agent=researcher
-)
+    Your final report should clearly articulate the key points,
+    impact, and future potential.
+    """,
+    expected_output='A comprehensive 3-4 paragraphs long report on the latest in {topic}.',
+    max_inter=3,
+    tools=[search_tool],
+    agent=researcher
+  )
 
-# Writing task based on research findings
-write_task = Task(
-  description=f"""Compose an insightful article on {topic}.
-  Focus on the latest trends and how it's impacting the industry.
-  This article should be easy to understand, engaging and positive.
-  """,
-  expected_output=f'A 4 paragraph article on {topic} advancements.',
-  tools=[search_tool],
-  agent=writer
-)
+  # Writing task based on research findings
+  write_task = Task(
+    description=f"""Compose an insightful article on {topic}.
+    Focus on the latest trends and how it's impacting society.
+    This article should be easy to understand, engaging, and positive.
+    """,
+    expected_output=f'A 4 paragraph article on {topic}.',
+    tools=[search_tool],
+    agent=writer
+  )
 
-from crewai import Crew, Process
+  from crewai import Crew, Process
 
-# Forming the tech-focused crew
-crew = Crew(
-  agents=[researcher, writer],
-  tasks=[research_task, write_task],
-  process=Process.sequential  # Sequential task execution
-)
+  # Forming the crew
+  crew = Crew(
+    agents=[researcher, writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential  # Sequential task execution
+  )
 
-# Starting the task execution process
-result = crew.kickoff()
-print(result)
+  # Starting the task execution process
+  result = crew.kickoff()
+  st.info(result)
+
+with st.form('my_form'):
+  topic = st.text_area('Enter text:', 'What should we research?')
+  submitted = st.form_submit_button('Submit')
+  if submitted:
+    generate_response(topic)
